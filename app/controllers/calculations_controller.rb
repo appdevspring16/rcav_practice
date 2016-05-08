6d24247bfb07ada867f942ef9a79392199c6bf06
Kenny Moyer <kmoyer@users.noreply.github.com>
2016-05-08 14:45:44 -0400

    kenny-moyer-initial-start

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,13 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def descriptive_statistics
+      @number = params[:number].to_i
+      @square = @number**2
+      @square_root = Math.sqrt(@number)
+
+
+      render("descriptive_statistics.html.erb")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/descriptive_statistics/new",     { :controller => "calculations", :action => "descriptive_statistics_form" })
+  get("/descriptive_statistics/results", { :controller => "calculations", :action => "descriptive_statistics" })
+
 end
