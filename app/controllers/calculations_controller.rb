aea6dd9989daf5aec10cbfbcb21d7351ecdfaebe
jmunoz0 <josemunoz@Joses-Air.zyxel.com>
2016-05-08 13:05:53 -0500

    done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,41 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+
+  def square_root
+@input=params[:user_input]
+
+@r=Math.sqrt(@input.to_i)
+@result=@r.round(2)
+
+render("square_root.html.erb")
+  end
+
+
+def random
+@min=params[:min]
+@max=params[:max]
+
+@random=rand(@min.to_i..@max.to_i)
+  end
+
+def payment
+r=params[:rate].to_f
+t=params[:period].to_i
+pr=params[:principal].to_i
+r_show=r/100
+@r_show=r_show.round(1)
+@t_show=t
+@r=((r/100)/12)/100
+@t=t*12
+@p=pr
+@interest=(1+@r)**@t
+
+@result=(@r*@p*@interest)/(@interest-1)
+@payment=@result.round(2)
+
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:user_input", :controller=>"calculations", :action=>"square_root")
+
+  get("/random/:min/:max", :controller=>"calculations", :action=>"random")
+
+  get("/payment/:rate/:period/:principal", :controller=>"calculations", :action=>"payment")
 end
