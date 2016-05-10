8f4247d5980fbc466c1d8af2f43fc2334a667b5c
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-05-09 20:34:12 -0500

    Starting to get a natural feel for this!!!

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,51 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def calculator
+
+  end
+  def no_form
+    @the_square = params[:the_square].to_i
+
+    @sr=Math.sqrt(@the_square)
+    render("square_root.html.erb")
+  end
+
+  def square_root
+    @the_square = params[:user_number].to_i
+
+    @sr=Math.sqrt(@the_square)
+  end
+
+  def no_form_rand
+    # User inputs two variables
+    @start = params[:start].to_i
+    @end = params[:end].to_i
+    @randnum = rand(@start..@end).to_i
+    # the calculator puts a number between those two
+    render("random.html.erb")
+  end
+
+  def randrange
+    # User inputs two variables
+    @start = params[:user_start].to_i
+    @end = params[:user_end].to_i
+    @randnum = rand(@start..@end).to_i
+    # the calculator puts a number between those two
+    render("random.html.erb")
+  end
+  def pmt
+
+    @apr=params[:apr]
+    @years=params[:years]
+    @princ=params[:princ]
+
+
+
+
+  end
+
+
+
 end
