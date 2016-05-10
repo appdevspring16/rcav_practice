d2fd1993ae727a03c480c16768e181c7fa646c25
shao313 <shaozhu@chicagobooth.edu>
2016-05-10 17:19:23 -0500

    complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,74 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_create_form
+
+
+  end
+
+  def square_calc
+
+    redirect_to("http://localhost:3000/square/#{params[:number]}")
+
+  end
+
+def square_show
+
+  @result = params[:id].to_f**2
+
 end
+
+def square_root_create_form
+
+
+end
+
+def square_root_calc
+
+  redirect_to("http://localhost:3000/square_root/#{params[:number]}")
+
+end
+
+def square_root_show
+
+@result = sprintf("%.2f", params[:id].to_f**0.5)
+
+end
+
+
+def random_create_form
+
+
+end
+
+def random_calc
+
+  redirect_to("http://localhost:3000/random/#{params[:min]}/#{params[:max]}")
+
+end
+
+def random_show
+
+@result = rand(params[:min].to_i..params[:max].to_i)
+
+end
+
+def payment_create_form
+
+
+end
+
+def payment_calc
+
+  redirect_to("http://localhost:3000/payment/#{params[:r]}/#{params[:n]}/#{params[:pv]}")
+
+end
+
+def payment_show
+
+@result = sprintf("%.2f",((params[:r].to_f/(10000*12))*params[:pv].to_f)/(1-(1+(params[:r].to_f/(10000*12)))**(-12*params[:n].to_f)))
+
+end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/instructions", { :controller => "calculations", :action => "instructions" })
+  get("/square", { :controller => "calculations", :action => "square_create_form" })
+  get("/square/submit", { :controller => "calculations", :action => "square_calc" })
+  get("/square/:id", { :controller => "calculations", :action => "square_show" })
+
+  get("/square_root", { :controller => "calculations", :action => "square_root_create_form" })
+  get("/square_root/submit", { :controller => "calculations", :action => "square_root_calc" })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root_show" })
+
+  get("/random", { :controller => "calculations", :action => "random_create_form" })
+  get("/random/submit", { :controller => "calculations", :action => "random_calc" })
+  get("/random/:min/:max", { :controller => "calculations", :action => "random_show" })
+
+  get("/payment", { :controller => "calculations", :action => "payment_create_form" })
+  get("/payment/submit", { :controller => "calculations", :action => "payment_calc" })
+  get("/payment/:r/:n/:pv", { :controller => "calculations", :action => "payment_show" })
+
+
+
+
+
+
 end
