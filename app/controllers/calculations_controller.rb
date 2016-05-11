a621ef4af704b99561b54f5445cb1fa4602d959b
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-10 20:15:46 -0500

    completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,36 @@
 class CalculationsController < ApplicationController
+
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    #The square of a particular NUMBER
+    @number = params[:number].to_i
+  end
+
+  def square_root
+    #The square root of a particular NUMBER
+    @number = params[:number].to_i
+    @square_root = Math.sqrt(@number)
+  end
+
+  def random
+    #A random number within a range, MIN to MAX
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @rand = rand(@max - @min) + @min
+  end
+
+  def loan_pmt
+    #The monthly loan payment, given an annual interest rate in basis points, or hundredths of a percent, a number of years, and a principal amount
+    #410 -> .041
+    @int_rate = params[:int_rate].to_f
+    @percent_int_rate = @int_rate / 10000.to_f
+    @monthly_int_rate = @percent_int_rate / 12.to_i
+    @years = params[:years].to_i
+    @principal = params[:principal].to_i
+    @monthly_pmt = '%.2f' % (@principal * ( @monthly_int_rate / (1 - (1 + @monthly_int_rate)**-@years))).round(2)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  #The square of a particular NUMBER lives at the URL /square/NUMBER
+  get("/square/:number", { :controller => "calculations", :action => "square" })
+  #The square root of a particular NUMBER lives at the URL /square_root/NUMBER
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
+  #A random number within a range, MIN to MAX, lives at the URL /random/MIN/MAX
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+  #The monthly loan payment, given an annual interest rate in basis points, or hundredths of a percent, a number of years, and a principal amount, lives at the #URL /payment/INTEREST_RATE/NUMBER_OF_YEARS/PRINCIPAL_VALUE
+  get("/payment/:int_rate/:years/:principal", { :controller => "calculations", :action => "loan_pmt" })
+
 end
