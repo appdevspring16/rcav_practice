0409d6f5810d4461136c44108d57dbc560adda3c
meghankinney <meghankinney@users.noreply.github.com>
2016-05-11 00:57:43 +0000

    finished square and squareroot

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,24 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root_new
+  end
+
+  def square_root_calculate
+
+    @number = params[:the_number].to_i
+    @root = @number.to_i**0.5
+  end
+
+  def square_new
+  end
+
+  def square_calculate
+
+    @number = params[:the_number].to_i
+    @square = @number.to_i**2
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  
+   #SQUARE
+  get("/square", { :controller => "calculations", :action => "square_new" })
+  get("/square/:number", { :controller => "calculations", :action => "square_calculate" })
+
+  #SQUARE ROOT
+  get("/square_root", { :controller => "calculations", :action => "square_root_new" })
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root_calculate" })
+
+ 
+
+
 end
