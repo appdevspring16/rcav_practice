15483f97b7758e6e13b4cb26e09d99483195ee07
meghankinney <meghankinney@users.noreply.github.com>
2016-05-11 02:31:14 +0000

    finishing up for now

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -8,27 +8,39 @@ class CalculationsController < ApplicationController
 
   def square_root_calculate
 
-    @number = params[:the_number].to_i
-    @root = @number.to_i**0.5
+    @number = params[:the_number].to_f
+    @root = @number**0.5
   end
 
   def square_new
   end
 
   def square_calculate
-
-    @number = params[:the_number].to_i
-    @square = @number.to_i**2
+    @number = params[:the_number].to_f
+    @square = @number**2
   end
 
   def random_new
   end
 
   def random_calculate
-    @min = params[:the_min].to_i
-    @max = params[:the_max].to_i
+    @min = params[:the_min].to_f
+    @max = params[:the_max].to_f
     @random = rand(@min..@max)
+  end
 
+  def payment_new
+  end
+
+  def payment_calculate
+
+    @rate = params[:the_interest_rate].to_f
+    @years = params[:the_number_of_years].to_i
+    @value = params[:the_principal_value].to_f
+
+    @r= @rate/100/12
+    @n= @years*12
+    @monthly_payment = ((@r*@value)*((1+@r)**@n))/(((1+@r)**@n)-1)
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
 
+   #LOAN PAYMENT
+  get("/payment", { :controller => "calculations", :action => "payment_new" })
+  get("/payment/:interest_rate/:number_of_years/:principal_value", { :controller => "calculations", :action => "payment_calculate" })
+
+  
     #MIN/MAX
   get("/random", { :controller => "calculations", :action => "random_new" })
   get("/random/:min/:max", { :controller => "calculations", :action => "random_calculate" })
-  
-  
+
+
    #SQUARE
   get("/square", { :controller => "calculations", :action => "square_new" })
   get("/square/:number", { :controller => "calculations", :action => "square_calculate" })
@@ -13,10 +18,10 @@ Rails.application.routes.draw do
   #SQUARE ROOT
   get("/square_root", { :controller => "calculations", :action => "square_root_new" })
   get("/square_root/:number", { :controller => "calculations", :action => "square_root_calculate" })
-  
-   #MIN/MAX
-  get("/random", { :controller => "calculations", :action => "random_new" })
-  get("/random/:min/:max", { :controller => "calculations", :action => "random_calculate" })
+
+ 
+
+
 
 
 
