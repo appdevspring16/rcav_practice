3da000c2ac54637ba0bb3ea1c96af3166d8fd802
Denis Bulanov <dbulanov@user.noreply.github.com>
2016-05-12 17:41:06 -0500

    Directors, create + index

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,26 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number = params[:number]
+    @number_squared = @number.to_i**2
+    render("square.html.erb")
+  end
+
+  def square_root
+    @number = params[:number]
+    @number_sqrt = @number.to_i**0.5
+    render("square_root.html.erb")
+  end
+
+  def random
+    @min = params[:min]
+    @max = params[:max]
+    range = (@min..@max).to_a
+    @random_n = range.sample
+  end
+
+  
+
 end
