1328cfd932cb9cdb4e9e9ca7d533200fb9ff63bb
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-05-12 15:39:03 -0500

    working

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @number = params[:number].to_f
+    @sqrt = (@number ** 0.5).round(2)
+  end
+
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @number = rand(@min..@max)
+  end
+
+  def payment
+    @apr = params[:apr].to_f/100
+    @years = params[:years].to_i
+    @principal = params[:principal].to_i
+
+    @num = (@apr/100/12) * @principal
+    @denom = 1-((1+(@apr/100/12))**(-1*@years*12))
+    @monthly_payment = @num/@denom
+  end
 end
