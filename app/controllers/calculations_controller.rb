8fa3eb08386ea8820f7daaad4f8c1f3211879781
Jesse C. Taylor <Jesse.Taylor@ChicagoBooth.edu>
2016-05-12 10:26:05 -0500

    Completed midterm calculator practice

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,26 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+  def sqr_rt
+@number=params[:number].to_f
+@sqr_rt=@number**0.5
+render("square_root")
+  end
+
+def random_number
+  @min=params[:min].to_f
+  @max=params[:max].to_f
+  @range= @max - @min
+  @random=rand(@range) + @min
+  render("random_number")
+end
+def payment
+  @bp=params[:bp].to_f
+  @yr=params[:yr].to_i
+  @principal=params[:principal].to_f
+  r = @bp/10000/12
+  n = @yr*12
+  @payment = (r*@principal)/(1-(1/(1+r)**n))
+render("payment")
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+#Landing page
   get("/", { :controller => "calculations", :action => "instructions" })
+
+#Square root
+get("/square_root/:number",{:controller=>"calculations",:action=>"sqr_rt"})
+
+#Random number
+get("/random/:min/:max",{:controller=>"calculations", :action=>"random_number"})
+
+#Loan Payment Calculator
+get("/payment/:bp/:yr/:principal",{:controller=>"calculations",:action=>"payment"})
+
 end
