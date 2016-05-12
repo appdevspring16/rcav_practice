a563fb226a014b115ba85d76af1890dd63f68317
afsaja <asaja@chicagobooth.edu>
2016-05-11 20:30:23 -0500

    Almost done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,23 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @square = params[:id].to_i
+    @root = @square**0.5
+    render("square_root.html.erb")
+  end
+
+  def square
+    @number = params[:id].to_i
+    @square = @number**2
+    render("square.html.erb")
+  end
+
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random = rand(@min..@max)
+    render("random.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+  get("/random/:min/:max", {:controller => "calculations", :action => "random"})
 end
