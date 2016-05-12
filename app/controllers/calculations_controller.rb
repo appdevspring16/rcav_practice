6d2b0deef13f051a2d2c79bec951e9d5a3d9c0e0
rbickham <rbickham@chicagobooth.edu>
2016-05-12 18:08:38 -0500

    rcb-rcav-practice-complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,34 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+
+    @first_number = params[:number_one].to_f
+
+    @square = Math.sqrt(@first_number).round(2)
+
+  end
+
+  def random
+
+    @first_number = params[:number_one].to_i
+    @second_number = params[:number_two].to_i
+
+    @random = rand(@first_number..@second_number)
+
+  end
+
+  def payment
+
+    @rate = params[:rate].to_d.round(2)/100
+    @years = params[:years].to_i
+    @principal = params[:principal].to_i
+
+    rate = @rate/(100*12)
+
+    @payment = ((rate +(rate/(((1+rate)**(@years*12))-1))) * @principal).round(2)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:number_one", { :controller => "calculations", :action => "square_root" })
+
+  get("/random/:number_one/:number_two", { :controller => "calculations", :action => "random" })
+
+  get("/payment/:rate/:years/:principal", { :controller => "calculations", :action => "payment" })
 end
