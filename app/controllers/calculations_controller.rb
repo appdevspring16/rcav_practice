f5f3936b077df2947325965fa53a3024daa140a4
Matthew Sidley <msidley@chicagobooth.edu>
2016-05-13 10:29:35 -0500

    Test

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,22 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+  def square
+    @number = params[:number].to_f
+    @square = (@number)**(0.5)
+    @square = @square.round(2)
+  end
+  def rand_num
+    @lownum = params[:number1].to_i
+    @highnum = params[:number2].to_i
+    @random_num = rand(@lownum...@highnum)
+  end
+  def payment
+    @interest_rate = (params[:interest_rate].to_f)/10
+    @number_of_years = params[:number_of_years].to_f
+    @principal_value = params[:principal_value].to_f
+    num = (@interest_rate/12)*@principal_value
+    den = (1-(1+@interest_rate)**(-12*@number_of_years))
+    @monthly_payment =(num/den).round(2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:number", { :controller => "calculations", :action => "square" })
+  get("/random/:number1/:number2", { :controller => "calculations", :action => "rand_num" })
+  get("/payment/:interest_rate/:number_of_years/:principal_value", { :controller => "calculations", :action => "payment" })
 end
