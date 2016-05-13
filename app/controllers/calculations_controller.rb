725f09f88fec4490d475b9b34628235e0def7290
mmuntazir <mehdi.muntazir@outlook.com>
2016-05-12 22:18:06 -0500

    Debugged

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+  def square_root
+    @sroot = params[:id].to_f
+    @srt = Math.sqrt(@sroot)
+  end
+  def square
+    @sroot = params[:id].to_f
+    @srt = @sroot*@sroot
+  end
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @rand = (@min..@max).to_a.sample
+  end
+  def payment
+    @rate = params[:rate].to_f/100
+    @term = params[:term].to_i
+    @bal = params[:bal].to_i
+    @mon_int = @rate/1200
+    @monthly_payment = (@bal * (@mon_int/(1-(1+@mon_int)**(@term*12*(-1))))).round(2)
+
+  end
 end
