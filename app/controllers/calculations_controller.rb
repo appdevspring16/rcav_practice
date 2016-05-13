d1f5a65490300191f80219379a3f03e4ae01c1c6
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-13 13:43:33 -0500

    stopping_point_1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,23 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number_to_square = Number.find(params[:number])
+    @number_to_square.square
+  end
+
+def square_root
+  @num = params[:number].to_i
+  @sq_root =  Math.sqrt(@num)
+  @round_root = @sq_root.round(2)
+  render("square_root.html.erb")
+ end
+
+ def random_number
+   @minimum = params[:min].to_i
+   @maximum = params[:max].to_i
+   @random = rand(@minimum..@maximum).to_i
+   render("random_number.html.erb")
+ end
 end
