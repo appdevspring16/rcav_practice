93712c8af369d6698ae2aeb4e6fc4fe25377763f
alrarruda20 <aarruda@chicagobooth.edu>
2016-05-13 18:20:30 -0500

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
+  def index
+    render("index.html.erb")
+  end
+
+  def square_root
+    @number = params[:number].to_f
+    @number_squareroot = Math.sqrt(@number)
+    render("square_root.html.erb")
+  end
+
+  def random_number
+    @number = params[:number].to_f
+    @number_random = sort_by { rand } 
+    render("random_number.html.erb")
+  end
+
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/index", { :controller => "calculations", :action => "index" })
+
+#square_root
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
+
+  #random_number
+    get("/random/:number/:number", { :controller => "calculations", :action => "random_number" })
+
+
+
 end
