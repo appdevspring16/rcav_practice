9810ee4e216eaba684fc094fac2e4cf9c868287d
alathans <alathans@users.noreply.github.com>
2016-05-13 08:51:17 -0500

    some updates

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -8,9 +8,18 @@ class CalculationsController < ApplicationController
     @number_root = @number.to_f**(0.5)
   end
 
-  def square_root
-    @number = params[:id].to_i
-    @number_root = @number.to_f**(0.5)
+  def random_number
+    @first_number = params[:first_number].to_i
+    @second_number = params[:second_number].to_i
+    @random_number = rand(@first_number..@second_number).to_i
+  end
+
+  def payment_calculator
+    @int_rate_bps = params[:int_rate_bps].to_f
+    @int = (@int_rate_bps/100)
+    @loan_term_yrs = params[:loan_term_yrs].to_i
+    @loan_amount = params[:loan_amount].to_f
+    @monthly_payment = (@int*@loan_amount*((1+@int)**@nper))/((1+@int)**@nper-1)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
 
 
-  get("/random/:first_number/:second_number", { :controller => "calculations", :action => "square_root" })
+  get("/random/:first_number/:second_number", { :controller => "calculations", :action => "random_number" })
+
+  get("/payment/:int_rate_bps/:loan_term_yrs/:loan_amount", { :controller => "calculations", :action => "payment_calculator" })
 
 end
