a38efcccd1fc1df5f06341e0fb97d211c765bbc6
Starr Marcello <starr.marcello@users.noreply.github.com>
2016-05-12 23:04:43 -0500

    More practice

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,16 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @num = params[:number]
+    @square_root = Math.sqrt(@num.to_i)
+
+  end
+
+  def random_number
+    @beg = params[:number1]
+    @end = params[:number2]
+    @rand = @beg.to_i + Random.rand((@end.to_i-@beg.to_i)+1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
+
+  get("/random/:number1/:number2", { :controller => "calculations", :action => "random_number" })
 end
