c98aaf85c78bb137c622ffb6d759adbc4dd2f34f
alathans <alathans@users.noreply.github.com>
2016-05-13 08:29:50 -0500

    first update

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,15 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @number = params[:id].to_i
+    @number_root = @number.to_f**(0.5)
+  end
+
+  def square_root
+    @number = params[:id].to_i
+    @number_root = @number.to_f**(0.5)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+
+
+  get("/random/:first_number/:second_number", { :controller => "calculations", :action => "square_root" })
+
 end
