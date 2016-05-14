13dcbeb83afdba34dc552de31d909d1e6b7eb4a0
Suresh Govindaraj <g123sk@gmail.com>
2016-05-13 22:05:33 -0500

    completed square_root

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,9 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @number      = params[:num].to_i
+    @square_root = Math.sqrt(@number).round(2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:num", { :controller => "calculations", :action => "square_root" })
 end
