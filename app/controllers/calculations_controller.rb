5ba381530f2b48b345b3ce66d769f0151d314815
Shana Dunnigan <dunniga5@users.noreply.github.com>
2016-05-13 19:54:55 -0500

    Added Routes and Sqrt Method

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,12 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+
+def sqrt
+  @square = params[:square].to_f
+  @sqrt = @square**0.5
+  render("sqrt.html.erb")
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+get("/square_root/:square", { :controller => "calculations", :action => "sqrt" })
+
+get("/square/:sqrt", { :controller => "calculations", :action => "square"})
+
+get("random/:min/:max", { :controller => "calculations", :action => "random"})
+
+get("/payment/:rate/:years/:principal", { :controller => "calculations", :action => "loan_payment"})
+
 end
