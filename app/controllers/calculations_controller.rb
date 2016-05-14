d4741a5e09d3e936abb14df8443da91078ecbd1d
Maayan <maharon@>
2016-05-14 09:08:31 -0500

    MA_rcav_practice_final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,24 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def squares
+    @number = params[:number].to_i
+    @square_root = @number**0.5
+  end
+
+  def random
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    @rand = rand(@number1..@number2)
+  end
+
+  def loan
+    @intrest = params[:intrest].to_f/100
+    @years = params[:years].to_i
+    @principal = params[:principal].to_i
+    @monthly_int = @intrest/(12*100)
+    @monthly_payment = (@principal * (@monthly_int/(1-(1+@monthly_int) ** (@years* -12.00))))  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get('/square_root/:number', { :controller => "calculations", :action => "squares" })
+
+  get('/random/:number1/:number2', { :controller => "calculations", :action => "random" })
+
+    get('/payment/:intrest/:years/:principal', { :controller => "calculations", :action => "loan" })
+
 end
