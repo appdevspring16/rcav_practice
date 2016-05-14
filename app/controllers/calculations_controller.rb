ff9ce34878bbf2902f45064702439d29d693d3f9
Ian Williams <iwilli1@users.noreply.github.com>
2016-05-14 12:38:50 -0500

    Random Number

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,4 +10,13 @@ def square_root
   render("square_root.html.erb")
 end
 
+def random_number
+  @first_number = params[:first_number]
+  @second_number = params[:second_number]
+@result = rand(@first_number.to_i..@second_number.to_i)
+
+
+render("random_number.html.erb")end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
   get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/random/:first_number/:second_number", { :controller => "calculations", :action => "random_number" })
   get("/", { :controller => "calculations", :action => "instructions" })
   get("/", { :controller => "calculations", :action => "instructions" })
 
